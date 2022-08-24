1fc4574ea0d3d4147be0bceb82e0be921a77b74c
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-08-24 16:18:26 +0200

    send and recieve data from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
+import { getAPI } from './Modules/adds.js';
+import form from './Modules/form.js';
+
+document.querySelector('.add').addEventListener('click', form);
+getAPI();
+document.querySelector('#btn-refresh').addEventListener('click', getAPI);
